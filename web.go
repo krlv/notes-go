55d93d46666a28816db1d47d75e5ffa7e3269ea9
krlv <eug.krlv@gmail.com>
2017-03-03 01:18:22 +0200

    Add Not Found Handler

--- web.go
+++ web.go
@@ -4,29 +4,50 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"log"
+	"strings"
+	"net/url"
 )
 
 const (
 	Port = ":8080"
 )
 
-func PageHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	slug, ok := vars["slug"]
+func getRoutePath(r *http.Request) (*url.URL, error) {
+	var pairs []string
 
-	if !ok {
-		slug = "home"
+	vars := mux.Vars(r)
+	if len(vars) > 0 {
+		pairs = make([]string, len(vars) * 2)
+		for  key, value := range vars {
+			pairs = append(pairs, key, value)
+		}
 	}
 
-	fileName := "public/" + slug + ".html"
+	return mux.CurrentRoute(r).URLPath(pairs...)
+}
+
+func PageHandler(w http.ResponseWriter, r *http.Request) {
+	path, _ := mux.CurrentRoute(r).GetPathTemplate()
+	path = strings.TrimLeft(path, "/")
+
+	fileName := "public/" + path + ".html"
 	log.Print("PageHandler:", fileName)
 
 	http.ServeFile(w, r, fileName)
 }
 
 func BlogHandler(w http.ResponseWriter, r *http.Request) {
+	path, _ := getRoutePath(r)
+
 	fileName := "public/blog.html"
-	log.Print("BlogHandler:", fileName)
+	log.Print("BlogHandler:", path)
+
+	http.ServeFile(w, r, fileName)
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	fileName := "public/404.html"
+	log.Print("NotFoundHandler:", r.RequestURI)
 
 	http.ServeFile(w, r, fileName)
 }
@@ -34,11 +55,13 @@ func BlogHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", PageHandler)
-	r.HandleFunc("/{slug}", PageHandler)
+	r.HandleFunc("/contacts", PageHandler)
 
 	b := r.PathPrefix("/blog").Subrouter()
 	b.HandleFunc("/", BlogHandler)
 	b.HandleFunc("/{slug}", BlogHandler)
+
+	r.NotFoundHandler = http.HandlerFunc(NotFoundHandler)
 
 	http.Handle("/", r)
 	http.ListenAndServe(Port,nil)
