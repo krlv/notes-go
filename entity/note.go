5ecdfb4b512ef093b29a5c7b6006bae6ee3f6d87
krlv <eug.krlv@gmail.com>
2018-03-08 23:08:09 +0200

    Notes: entity, templates & routes

---
+++ entity/note.go
@@ -0,0 +1,9 @@
+package entity
+
+// Note
+type Note struct {
+	Id int
+	Title string
+	Body string
+	Tags []string
+}
--- web.go
+++ web.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"net/http"
+	"github.com/icrowley/fake"
+	"github.com/krlv/goweb/entity"
 	"log"
-	"strings"
+	"math/rand"
+	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
 	"text/template"
 )
 
@@ -18,8 +22,8 @@ func getRoutePath(r *http.Request) (*url.URL, error) {
 
 	vars := mux.Vars(r)
 	if len(vars) > 0 {
-		pairs = make([]string, len(vars) * 2)
-		for  key, value := range vars {
+		pairs = make([]string, len(vars)*2)
+		for key, value := range vars {
 			pairs = append(pairs, key, value)
 		}
 	}
@@ -41,6 +45,34 @@ func PageHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, fileName)
 }
 
+func NotesHandler(w http.ResponseWriter, r *http.Request) {
+	path, _ := getRoutePath(r)
+	log.Print("NotesHandler:", path)
+
+	notes := make([]entity.Note, 2)
+	for i := 0; i < cap(notes); i++ {
+		note := new(entity.Note)
+		note.Title = fake.Sentence()
+		note.Body = fake.Paragraphs()
+
+		note.Tags = make([]string, rand.Intn(3))
+		for j := 0; j < cap(note.Tags); j++ {
+			note.Tags[j] = fake.Word()
+		}
+
+		notes[i] = *note
+	}
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	if id > 0 {
+		t, _ := template.ParseFiles("templates/note.html")
+		t.Execute(w, notes[id])
+	} else {
+		t, _ := template.ParseFiles("templates/notes.html")
+		t.Execute(w, notes)
+	}
+}
+
 func BlogHandler(w http.ResponseWriter, r *http.Request) {
 	path, _ := getRoutePath(r)
 	log.Print("BlogHandler:", path)
@@ -49,7 +81,7 @@ func BlogHandler(w http.ResponseWriter, r *http.Request) {
 	slug = strings.Title(slug)
 
 	t, _ := template.ParseFiles("templates/blog.html")
-	t.Execute(w, map[string]interface{}{ "Title": slug })
+	t.Execute(w, map[string]interface{}{"Title": slug})
 }
 
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
@@ -57,13 +89,16 @@ func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
 	path := strings.TrimLeft(r.RequestURI, "/")
 
 	t, _ := template.ParseFiles("templates/404.html")
-	t.Execute(w, map[string]interface{}{ "Path": path })
+	t.Execute(w, map[string]interface{}{"Path": path})
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", PageHandler)
-	r.HandleFunc("/contacts", PageHandler)
+
+	n := r.PathPrefix("/notes").Subrouter()
+	n.HandleFunc("/", NotesHandler)
+	n.HandleFunc("/{id:[0-9]+}", NotesHandler)
 
 	b := r.PathPrefix("/blog").Subrouter()
 	b.HandleFunc("", BlogHandler)
@@ -72,5 +107,5 @@ func main() {
 	r.NotFoundHandler = http.HandlerFunc(NotFoundHandler)
 
 	http.Handle("/", r)
-	http.ListenAndServe(Port,nil)
+	http.ListenAndServe(Port, nil)
 }
