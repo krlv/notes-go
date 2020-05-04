188a9ea2f94a078a5a6d99c693ed1e73444cba0d
eugene kirillov <eug.krlv@gmail.com>
2020-05-03 18:09:02 -0700

    fix: use struct pointers in repositories

--- pkg/note/repository.go
+++ pkg/note/repository.go
@@ -3,8 +3,8 @@ package note
 // Repository is an adapter for persistence implementation (port interface)
 type Repository interface {
 	// FindNotes returns list of existing notes
-	FindNotes() []Note
+	FindNotes() []*Note
 
 	// GetNoteByID returns note by ID or error if note not found
-	GetNoteByID(int) (Note, error)
+	GetNoteByID(int) (*Note, error)
 }
--- pkg/storage/memory.go
+++ pkg/storage/memory.go
@@ -11,19 +11,19 @@ import (
 
 // MemoryStorage repository
 type MemoryStorage struct {
-	pages map[string]blog.Page
-	notes map[int]note.Note
+	pages map[string]*blog.Page
+	notes map[int]*note.Note
 }
 
 // NewStorage creates new repository
 func NewStorage() *MemoryStorage {
 	maxPages := 3
 
-	pages := make(map[string]blog.Page, maxPages)
+	pages := make(map[string]*blog.Page, maxPages)
 	for i := 0; i < maxPages; i++ {
 		slug := "slug-" + strconv.Itoa(i+1)
 
-		pages[slug] = blog.Page{
+		pages[slug] = &blog.Page{
 			ID:    i + 1,
 			Slug:  slug,
 			Title: fake.Sentence(),
@@ -32,14 +32,14 @@ func NewStorage() *MemoryStorage {
 	}
 
 	maxNotes, maxTags := 2, 3
-	notes := make(map[int]note.Note, maxNotes)
+	notes := make(map[int]*note.Note, maxNotes)
 	for i := 0; i < maxNotes; i++ {
 		tags := make([]string, rand.Intn(maxTags))
 		for j := 0; j < cap(tags); j++ {
 			tags[j] = fake.Word()
 		}
 
-		notes[i] = note.Note{
+		notes[i] = &note.Note{
 			ID:    i,
 			Title: fake.Sentence(),
 			Body:  fake.Paragraphs(),
@@ -54,8 +54,8 @@ func NewStorage() *MemoryStorage {
 }
 
 // GetPageBySlug returns page by it's slug
-func (s *MemoryStorage) FindPages() []blog.Page {
-	pages := make([]blog.Page, 0, len(s.pages))
+func (s *MemoryStorage) FindPages() []*blog.Page {
+	pages := make([]*blog.Page, 0, len(s.pages))
 
 	for _, page := range s.pages {
 		pages = append(pages, page)
@@ -65,13 +65,13 @@ func (s *MemoryStorage) FindPages() []blog.Page {
 }
 
 // GetPageBySlug returns page by it's slug
-func (s *MemoryStorage) GetPageBySlug(slug string) (blog.Page, error) {
+func (s *MemoryStorage) GetPageBySlug(slug string) (*blog.Page, error) {
 	return s.pages[slug], nil
 }
 
 // FindNotes returns notes from memory repository
-func (s *MemoryStorage) FindNotes() []note.Note {
-	notes := make([]note.Note, 0, len(s.notes))
+func (s *MemoryStorage) FindNotes() []*note.Note {
+	notes := make([]*note.Note, 0, len(s.notes))
 
 	for _, page := range s.notes {
 		notes = append(notes, page)
@@ -81,6 +81,6 @@ func (s *MemoryStorage) FindNotes() []note.Note {
 }
 
 // GetNoteByID returns note by ID or error if note not found in memory repo
-func (s *MemoryStorage) GetNoteByID(id int) (note.Note, error) {
+func (s *MemoryStorage) GetNoteByID(id int) (*note.Note, error) {
 	return s.notes[id], nil
 }
